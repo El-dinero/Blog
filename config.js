d285a3205ca5c7952a03c181db7df3c3e21092ae
El-dinero <mironenkovasila@gmail.com>
2020-04-18 02:17:52 +0300

    Skilet project fixed

--- config.js
+++ config.js
@@ -1,4 +1,11 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+const root = path.join.bind(this, __dirname);
+dotenv.config({ path: root(".env") });
+
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    MONGO_URL: 'mongodb+srv://vasko1234:vasko1234@conect-okoar.mongodb.net/blog?retryWrites=true&w=majority'
-}
+  PORT: process.env.PORT || 3000,
+  MONGO_URL: process.env.MONGO_URL,
+  IS_PRODUCTION: process.env.NODE_ENV === "production",
+};
