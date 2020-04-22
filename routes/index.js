9ed1bd7aaef2184df6eeeed5b7d92144b8ca0ca0
El-dinero <mironenkovasila@gmail.com>
2020-04-23 00:08:39 +0300

    Post one post criate finish

--- routes/index.js
+++ routes/index.js
@@ -27,13 +27,47 @@ function posts(req, res) {
             },
           });
         })
-        .catch(console.log);
+        .catch(() => {
+          throw new Error("Server Error");
+        });
     })
-    .catch(console.log);
+    .catch(() => {
+      throw new Error("Server Error");
+    });
 }
 
 // routers
 router.get("/", (req, res) => posts(req, res));
 router.get("/archive/:page", (req, res) => posts(req, res));
+router.get("/posts/:post", (req, res, next) => {
+  const userId = req.session.userId;
+  const userLogin = req.session.userLogin;
+  const url = req.params.post.trim().replace(/ +(?= )/g, "");
+
+  if (!url) {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  } else {
+    Post.findOne({
+      url,
+    }).then((post) => {
+      if (!post) {
+        const err = new Error("Not Found");
+        err.status = 404;
+        next(err);
+      } else {
+        res.render("post/onePost", {
+          title: "OnePost",
+          post,
+          user: {
+            id: userId,
+            login: userLogin,
+          },
+        });
+      }
+    });
+  }
+});
 
 module.exports = router;
