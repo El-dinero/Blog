9c5b13255c1cc8dffcc9ea6cdc2694059f7d6edd
El-dinero <mironenkovasila@gmail.com>
2020-05-20 21:59:43 +0300

    Deploy

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 const root = path.join.bind(this, __dirname);
 dotenv.config({ path: root(".env") });
-
 module.exports = {
   PORT: process.env.PORT || 3000,
   MONGO_URL: process.env.MONGO_URL,
