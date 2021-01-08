aecf9241d26331d15f03182a1c4dc2b536451c58
VasilyUA <mironenkovasila@gmail.com>
2021-01-08 19:57:20 +0200

    hotfix deplou

--- app.js
+++ app.js
@@ -1,82 +1,85 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const staticAsset = require("static-asset");
-const mongoose = require("mongoose");
-const session = require("express-session");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const staticAsset = require('static-asset');
+const mongoose = require('mongoose');
+const session = require('express-session');
 
-const MongoStore = require("connect-mongo")(session);
-const config = require("./config");
+const MongoStore = require('connect-mongo')(session);
+const config = require('./config');
 //EXPRESS
 const app = express();
 
 //database
 mongoose.Promise = global.Promise;
-mongoose.set("debug", config.IS_PRODUCTION);
+// mongoose.set("debug", config.IS_PRODUCTION);
 mongoose.connection
-  .on("error", (error) => console.log(error))
-  .on("close", () => console.log("Database connection..."))
-  .once("open", () => {
-    const info = mongoose.connections[0];
-    console.log(`Connected to Host:${info.host}:${info.port}/${info.name}`);
-    //Autopost + 10
-    // require("./mocks")();
-    //Listen Express
-    app.listen(config.PORT, () => {
-      console.log(`Server started on port ${config.PORT}!`);
-    });
-  });
+	.on('error', (error) => console.log(error))
+	.on('close', () => console.log('Database connection...'))
+	.once('open', () => {
+		const info = mongoose.connections[0];
+		console.log(
+			`Connected to Host:${info.host}:${info.port}/${info.name}`
+		);
+		//Autopost + 10
+		// require("./mocks")();
+		//Listen Express
+	});
 mongoose.connect(config.MONGO_URL, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 // sessions
 app.use(
-  session({
-    secret: config.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-    }),
-  })
+	session({
+		secret: config.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: false,
+		store: new MongoStore({
+			mongooseConnection: mongoose.connection,
+		}),
+	})
 );
 
 //status and uses
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(staticAsset(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "pablic")));
-app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use(staticAsset(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pablic')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(
-  "/javascripts",
-  express.static(path.join(__dirname, "node_modules", "jquery", "dist"))
+	'/javascripts',
+	express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
 );
 
 //routes
-app.use("/", require("./routes/index"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/post", require("./routes/post"));
-app.use("/users", require("./routes/user"));
-app.use("/comment", require("./routes/comment"));
+app.use('/', require('./routes/index'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/post', require('./routes/post'));
+app.use('/users', require('./routes/user'));
+app.use('/comment', require('./routes/comment'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 // eslint-disable-next-line no-unused-vars
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.render("error", {
-    message: error.message,
-    error: !config.IS_PRODUCTION ? error : {},
-  });
+	res.status(error.status || 500);
+	res.render('error', {
+		message: error.message,
+		error: !config.IS_PRODUCTION ? error : {},
+	});
 });
+
+app.listen(config.PORT, () =>
+	console.log(`Server started on port ${config.PORT}!`)
+);
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
-const dotenv = require("dotenv");
-const path = require("path");
+const dotenv = require('dotenv');
+const path = require('path');
 
 const root = path.join.bind(this, __dirname);
-dotenv.config({ path: root(".env") });
+dotenv.config({ path: root('.env') });
 module.exports = {
-  PORT: process.env.PORT || 3000,
-  MONGO_URL: process.env.MONGO_URL,
-  SESSION_SECRET: process.env.SESSION_SECRET,
-  IS_PRODUCTION: process.env.NODE_ENV === "production",
-  PER_PAGE: process.env.PER_PAGE,
+	PORT: process.env.PORT,
+	MONGO_URL: process.env.MONGO_URL,
+	SESSION_SECRET: process.env.SESSION_SECRET,
+	IS_PRODUCTION: process.env.NODE_ENV,
+	PER_PAGE: process.env.PER_PAGE,
 };
